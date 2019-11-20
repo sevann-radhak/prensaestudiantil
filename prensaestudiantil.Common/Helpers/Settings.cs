1286b92dae0fe3da7ec1ff3523f0b6e61acac752
Sevann Radhak <sevann.radhak@gmail.com>
2019-11-20 18:00:34 -0300

    i#12 Mobile first part

---
+++ prensaestudiantil.Common/Helpers/Settings.cs
@@ -0,0 +1,19 @@
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+
+namespace prensaestudiantil.Common.Helpers
+{
+    public static class Settings
+    {
+        //private const string _propertyImages = "PropertyImages";
+        private static readonly string _settingsDefault = string.Empty;
+
+        private static ISettings AppSettings => CrossSettings.Current;
+
+        //public static string PropertyImages
+        //{
+        //    get => AppSettings.GetValueOrDefault(_propertyImages, _settingsDefault);
+        //    set => AppSettings.AddOrUpdateValue(_propertyImages, value);
+        //}
+    }
+}
