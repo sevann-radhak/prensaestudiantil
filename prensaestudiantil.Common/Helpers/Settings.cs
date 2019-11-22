63f5cd20d0abd259369ef2efad00f71cf41fc2b1
Sevann Radhak <sevann.radhak@gmail.com>
2019-11-22 00:02:40 -0300

    i#13 Merge

--- prensaestudiantil.Common/Helpers/Settings.cs
+++ prensaestudiantil.Common/Helpers/Settings.cs
@@ -5,15 +5,22 @@ namespace prensaestudiantil.Common.Helpers
 {
     public static class Settings
     {
-        //private const string _propertyImages = "PropertyImages";
-        private static readonly string _settingsDefault = string.Empty;
+        private const string _token = "token";
+        private const string _user = "user";
+        private static readonly string _stringDefault = string.Empty;
 
         private static ISettings AppSettings => CrossSettings.Current;
 
-        //public static string PropertyImages
-        //{
-        //    get => AppSettings.GetValueOrDefault(_propertyImages, _settingsDefault);
-        //    set => AppSettings.AddOrUpdateValue(_propertyImages, value);
-        //}
+        public static string Token
+        {
+            get => AppSettings.GetValueOrDefault(_token, _stringDefault);
+            set => AppSettings.AddOrUpdateValue(_token, value);
+        }
+
+        public static string User
+        {
+            get => AppSettings.GetValueOrDefault(_user, _stringDefault);
+            set => AppSettings.AddOrUpdateValue(_user, value);
+        }
     }
 }
